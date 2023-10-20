f6a56961946cacf838d0d88c3de75384053fd0bd
alifhasanshah <alifhasanshah@gmail.com>
2023-10-20 13:49:49 +0600

    mycart post api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5001;
@@ -17,46 +17,54 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
     const database = client.db("productsDB");
     const productsCollection = database.collection("products");
+    const myCartCollection = database.collection("myCart");
 
-    app.get("/products/:brandname", async(req, res) => {
+    app.get("/products/:brandname", async (req, res) => {
       const brandname = req.params.brandname;
       const query = { brand: brandname };
       const cursor = productsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get('/product/:id', async(req, res) => {
+    app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const product = await productsCollection.findOne(query);
       res.send(product);
-    })
+    });
 
-    app.post("/products", async(req, res) => {
+    app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productsCollection.insertOne(product);
       res.send(result);
-    })
-    
+    });
+
+    // my cart api
+    app.post("/myCart", async (req, res) => {
+      const myCart = req.body;
+      const result = await myCartCollection.insertOne(myCart);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Electon server is running successfully");
