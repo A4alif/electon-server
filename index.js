874a8721c372067a30b569be3d89c292375ea20f
alifhasanshah <alifhasanshah@gmail.com>
2023-10-20 13:02:47 +0600

    single product get api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5001;
@@ -33,6 +33,13 @@ async function run() {
       const cursor = productsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    app.get('/product/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const product = await productsCollection.findOne(query);
+      res.send(product);
     })
 
     app.post("/products", async(req, res) => {
